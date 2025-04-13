6a89a1bc2a1492f90a40f324f71063024c2e5698
gabriel <chechi011619@gmail.com>
2025-04-13 19:16:54 -0300

    fix: .env

--- src/firebase/firebaseAdmin.ts
+++ src/firebase/firebaseAdmin.ts
@@ -2,9 +2,9 @@
 import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getStorage } from "firebase-admin/storage";
 
+// 🔄 Converte a string da env, tratando as quebras de linha
 const rawServiceAccount = process.env.FIREBASE_SERVICE_ACCOUNT_KEY!;
-const serviceAccount = JSON.parse(rawServiceAccount.replace(/\\n/g, "\n"));
-
+const serviceAccount = JSON.parse(rawServiceAccount.replace(/\\n/g, '\n'));
 
 if (!getApps().length) {
   initializeApp({
