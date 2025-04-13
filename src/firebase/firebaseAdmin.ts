1cf796cd48c1bc3415edf11171425e72d48de0bc
gabriel <chechi011619@gmail.com>
2025-04-13 19:00:07 -0300

    fix: .env

--- src/firebase/firebaseAdmin.ts
+++ src/firebase/firebaseAdmin.ts
@@ -2,7 +2,7 @@
 import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getStorage } from "firebase-admin/storage";
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY!);
 
 if (!getApps().length) {
   initializeApp({
